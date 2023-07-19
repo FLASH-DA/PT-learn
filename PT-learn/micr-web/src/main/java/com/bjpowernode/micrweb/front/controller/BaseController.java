2abe9a0fc3a8e7fb486879e1217ccf525fd83e02
flash <2483263131@qq.com>
2023-07-19 20:57:08 +0800

    v1

---
+++ PT-learn/micr-web/src/main/java/com/bjpowernode/micrweb/front/controller/BaseController.java
@@ -0,0 +1,10 @@
+package com.bjpowernode.micrweb.front.controller;
+
+/**
+ * @author csd
+ * @version 1.0
+ */
+@SuppressWarnings({"all"})
+public class BaseController {
+
+}
---
+++ PT-learn/micr-web/src/main/java/com/bjpowernode/micrweb/front/controller/PlatInfoController.java
@@ -0,0 +1,18 @@
+package com.bjpowernode.micrweb.front.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author csd
+ * @version 1.0
+ */
+@SuppressWarnings({"all"})
+@RestController
+public class PlatInfoController extends BaseController {
+    /**
+     * 平台基本信息
+     */
+    public void queryPaltInfo(){
+
+    }
+}
